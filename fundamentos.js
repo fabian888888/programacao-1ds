f7a104ea5569670050bf085e6d372e1295d832a7
fabian888888 <fabian.rivas@escola.pr.gov.br>
2025-11-28 14:50:36 -0300

    Add online compiler links to fundamentos.js
    Added multiple links to online compiler resources.

---
+++ fundamentos.js
@@ -0,0 +1,12 @@
+
+https://www.programiz.com/online-compiler/7puNp9saEYyGl
+
+https://www.programiz.com/online-compiler/9n6inXK9amAaq
+
+https://www.programiz.com/online-compiler/7hbahDdy9qlKC
+https://www.programiz.com/online-compiler/2lKLlEUerXfFo
+https://www.programiz.com/online-compiler/8i7ui6L3MPXRp
+https://www.programiz.com/online-compiler/2UzxUTYNEZzc4
+https://www.programiz.com/online-compiler/2O0hOGZr2KCYk
+
+https://www.programiz.com/online-compiler/3oGOoCjHxE9WW
